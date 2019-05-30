53f6c3d5e8cf10f7dcc72f2faf09d978c2c86d6d
祁美杰 <15730628810@163.com>
2019-05-30 11:02:32 +0800

    first commit

---
+++ dy01/dase_type.py
@@ -0,0 +1,111 @@
+cc = 20.5
+print('Hello World')
+de = 5
+def int_dmo():
+    t = 123
+    print(type(t))
+def str_dmo():
+    k='123'
+    print(type(k))
+
+
+
+def tx():
+    t = 45
+    print(type(t))
+
+def qm():
+    q = 'qweed'
+    print(type(q))
+
+# if __name__ == '__main__':
+#     print('Hello World')
+#     int_dmo()
+#     str_dmo()
+#     tx()
+#     qm()
+
+#  zhuoye
+
+def def1():
+    print('def1')
+
+def def2():
+    print('def2')
+
+def def3():
+    print('def3')
+
+def xs():
+    qq = 2.25
+    print(type(qq))
+
+def ff():
+    du = 20.5
+    print(type(du))
+
+def www():
+    ed = 'jfkkkf'
+    print(type(ed))
+
+def wertt():
+    d = '25'
+    print(type(d))
+    d = int(d)
+    print(type(d))
+
+def erty():
+    q = 12.5
+    print(type(q))
+    q = int(q)
+    print(type(q))
+
+
+def qqww():
+    q = 147
+    w = 'gagaga'
+    r = 'xiaomifen'
+    print(w + r)
+    print('%s%s'%(q,w))
+
+def rty():
+    s=12.5
+    er=32.5
+    rt='123'
+    print(s+er)
+    print('%s,%s,%s'%(s,er,rt))
+
+def ff(num1,num2):
+    print(num1)
+    print(num2)
+    print(num1+num2)
+    return num1+num2
+
+def qq(q,t):
+    print(q)
+    print(t)
+    print(q+t)
+    return q+t
+
+def ww(d,e):
+    print(d)
+    print(e)
+    print(d+e)
+    return d + e
+
+q='乔沃维奇大家聚集'
+
+
+if __name__ == '__main__':
+    # def3()
+    # def1()
+    # def2()
+    # print(de)
+    # xs()
+    # print(q[1:5])
+    # print(q[-1:])
+    # print(q[1:2])
+    # print(q[2:5])
+    # print(q[-5:-1])
+    # print(q[2])
+    print(q[::-1])
---
+++ dy01/pythonqq.py
@@ -0,0 +1,7 @@
+from dy01 import dase_type
+
+if __name__ == '__main__':
+    srtqq = dase_type.qq(5, 5)
+    g = srtqq
+    print(g)
+
